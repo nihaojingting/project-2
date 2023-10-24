85dc0d31006ec70efff907e04b346b5d21cc2f99
Jing-Ting Dong <stevendong2002@gmail.com>
2023-10-24 11:19:26 -0400

    changes

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -22,8 +22,6 @@ export class PolarisChip extends LitElement {
         height: auto;
         border: 2px solid #444; /* Default border color */
         transition: filter 0.3s; /* Add a smooth transition */
-        width: 900px;
-        height: 400px;
       }
 
       a.chip:hover img {
