07880c28918367a69084aa0a6d956e352e34f61b
Jing-Ting Dong <stevendong2002@gmail.com>
2023-10-24 12:13:12 -0400

    blue background

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -30,7 +30,7 @@ export class PolarisChip extends LitElement {
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: rgba(blue, 0.4); /* Navy blue with 40% opacity */
+        background-color: rgba(0, 0, 255, 0.4); /* Navy blue with 50% opacity */
         opacity: 0; /* Initially transparent */
       }
 
