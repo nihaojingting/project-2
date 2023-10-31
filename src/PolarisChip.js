09d7a13b61875e77fcee7a7a3fab87ae6f0120df
Jing-Ting Dong <stevendong2002@gmail.com>
2023-10-31 13:12:13 -0400

    p much done

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -1,141 +1,333 @@
 import { LitElement, html, css } from 'lit';
-import './date-chip.js';
+
+import "./date-chip.js";
+
+ 
 
 export class PolarisChip extends LitElement {
+
   static get properties() {
+
     return {
+
       name: { type: String },
+
       link: { type: String },
+
       active: { type: Boolean, reflect: true },
+
       imageSrc: { type: String },
+
       header: { type: String },
+
       desc: { type: String },
+
+      date: { type: String },
+
     };
+
   }
+
+ 
 
   static get styles() {
+
     return css`
+
       :host {
+
         display: inline-flex;
+
         margin: 12px;
+
         max-width: 300px;
+
         flex-direction: column;
+
       }
+
+ 
+
+      :host {
+
+        display: inline-flex;
+
+        margin: 12px;
+
+        max-width: 450px;
+
+        flex-direction: column;
+
+      }
+
+ 
 
       .cardcontainer {
+
         display: flex;
+
         flex-direction: column;
+
+        float: left;
+
         box-sizing: border-box;
+
         font-family: 'Roboto', sans-serif;
+
         margin-right: 10px;
-        line-height: 24px;
-        align-items: flex-start; /* Add this line */
-}
+
+        line-height: 20px;
+
+        gap: 0px;
+
+      }
+
+ 
 
       a.chip {
+
         position: relative;
+
         display: block;
+
         cursor: pointer;
-        font-weight: bold;
-        text-decoration: none;
-        padding: 0;
-        font-size: 16px;
+
       }
+
+ 
 
       a.chip::before {
+
         content: '';
+
         position: absolute;
+
         top: 0;
+
         left: 0;
+
         width: 100%;
+
         height: 100%;
-        background-color: rgba(30, 64, 124, 0.35); 
-        opacity: 0; 
+
+        background-color: rgba(30, 64, 124, 0.35);
+
+        opacity: 0;
+
       }
+
+ 
 
       a.chip:hover::before {
-        opacity: 1; 
+
+        opacity: 1;
+
       }
+
+ 
 
       a.chip img {
-        max-width: 100%;
+
+        max-width: 450px;
+
         height: auto;
+
       }
+
+ 
+
+      a.chip {
+
+        font-weight: bold;
+
+        text-decoration: none;
+
+        padding: 0px 0px;
+
+        font-size: 16px;
+
+      }
+
+ 
 
       .imagecontainer {
+
         display: flex;
+
         flex-wrap: wrap;
-        justify-content: center; 
-        align-items: center; 
+
+        justify-content: center;
+
+        align-items: center;
+
       }
 
-      .headercontainer {
-  display: flex;
-  margin: 20px 0;
-  align-items: flex-start; /* Add this line */
-}
-
-
-      h3 {
-        font-size: 20.8px;
-        text-transform: capitalize;
-      }
-
-      .cardheader {
-        color: rgb(0, 95, 169);
-        text-decoration: none;
-      }
-
-      .descriptioncontainer {
-        padding: 10px;
-      }
-
-      .description {
-        font-size: 19.2px;
-        color: black;
-        display: flex; /* Add this line */
-        align-items: flex-start; /* Add this line */
-      }
-
+ 
 
       @media (max-width: 768px) {
+
         .cardcontainer {
+
           flex-direction: column;
+
         }
+
       }
+
+ 
+
+      .headercontainer {
+
+        display: flex;
+
+        margin: 10px 0;
+
+        align-items: stretch;
+
+        gap: 10px;
+
+    }
+
+      h3 {
+
+        font-size: 20.8px;
+
+        font-family: sans-serif;
+
+        margin-block-start: 0em;
+
+        margin-block-end: 0em;
+
+        font-weight: 400;
+
+ 
+
+      }
+
+ 
+
+      .cardheader {
+
+        color: rgb(0, 95, 169);
+
+        text-decoration-line: none;
+
+        text-decoration-style: solid;
+
+      }
+
+ 
+
+      .descriptioncontainer {
+
+        padding-left: -2px;
+
+        padding-right: 1px;
+
+        padding-top: 0em;
+
+        padding-bottom: 10px;
+
+        line-height: 24px;
+
+        margin-top: 0px;
+
+      }
+
+ 
+
+      .description {
+
+        font-family: 'Roboto', sans-serif;
+
+        font-size: 19.2px;
+
+        color: #000;
+
+        font-weight: 150;
+
+    }
+
     `;
+
   }
+
+ 
 
   constructor() {
+
     super();
+
     this.name = '';
+
     this.link = '';
+
     this.imageSrc = '';
+
     this.header = '';
+
     this.desc = '';
+
+    this.date = '';
+
   }
 
+ 
+
   render() {
+
     return html`
-      <div class="cardcontainer">
+
+    <div class="cardcontainer">
+
+      <!-- image container -->
+
+      <div class="imagecontainer">
+
         <a class="chip" href="${this.link}" target="_blank">
-          <img src="${this.imageSrc}" alt="${this.name}" />
+
+          <img src="${this.imageSrc}" alt="${this.name}">
+
           <slot>${this.name}</slot>
+
         </a>
-        <div class="headercontainer">
-          <date-chip></date-chip>
-          <h3>
-            <a class="cardheader" href="${this.link}" target="_blank">
-              <slot>${this.header}</slot>
-            </a>
-          </h3>
-        </div>
-        <div class="descriptioncontainer">
-          <p class="description">
-            <slot>${this.desc}</slot>
-          </p>
-        </div>
+
       </div>
+
+      <!-- date chip / heading -->
+
+      <div class="headercontainer">
+
+        <date-chip date="${this.date}"></date-chip>
+
+        <h3>
+
+          <a class="cardheader" href="${this.link}" target="_blank">
+
+            <slot>${this.header}</slot>
+
+          </a>
+
+        </h3>
+
+      </div>
+
+      <!-- desc -->
+
+      <div class="descriptioncontainer">
+
+        <p class="description">
+
+          <slot>${this.desc}</slot>
+
+        </p>
+
+      </div>
+
+    </div>
+
     `;
+
   }
+
 }
--- src/date-chip.js
+++ src/date-chip.js
@@ -1,96 +1,217 @@
 import { LitElement, html, css } from 'lit';
 
+ 
+
 export class DateChip extends LitElement {
+
+ 
+
   static get properties() {
+
+ 
+
     return {
+
       date: { type: String },
+
       month: { type: String },
+
       day: { type: Number }
+
     };
+
   }
+
+ 
 
   static get styles() {
+
+ 
+
     return css`
+
+ 
+
       :host {
+
         display: block;
+
       }
+
+ 
+
       .container {
+
         display: flex;
+
         flex-direction: column;
+
         float: left;
+
         box-sizing: border-box;
+
         font-family: 'Roboto', sans-serif;
+
+        width: 100%;
+
+        height: 100%;
+
         margin-right: 10px;
+
+        margin-top: 0em;
+
+        margin-left: -6px;
+
         line-height: 24px;
+
         text-align: center;
+
       }
+
+ 
+
       .month {
+
         background-color: #1E407C;
+
         background-clip: border-box;
+
         background-origin: padding-box;
+
         background-size: auto;
+
         border-bottom-left-radius: 2px;
+
         border-bottom-right-radius: 2px;
+
         border-top-left-radius: 0px;
+
         border-top-right-radius: 0px;
+
         color: white;
-        font-size: 12.8px;
+
+        font-size: 14px;
+
         font-weight: 700;
+
         line-height: 23.04px;
+
         padding-left: 10px;
+
         padding-right: 10px;
-        padding-top: 1px;
+
+        padding-top: 2px;
+
         padding-bottom: 1px;
+
         text-transform: uppercase;
+
       }
+
+ 
+
       .day {
-        background-color: white;
+
+        background-color: #f3f3f3;
+
         background-clip: border-box;
+
         background-origin: padding-box;
+
         background-size: auto;
+
         border-bottom-left-radius: 2px;
+
         border-bottom-right-radius: 2px;
+
         border-top-left-radius: 0px;
+
         border-top-right-radius: 0px;
+
         color: #444444;
+
         font-size: 18px;
+
         font-weight: 900;
+
         line-height: 27px;
+
         padding-left: 20px;
+
         padding-right: 20px;
+
         padding-top: 10px;
+
         padding-bottom: 10px;
+
       }
+
     `;
+
   }
+
+ 
 
   constructor() {
+
     super();
+
     this.date = '01/01/2001'
+
     this.month = 'Jan';
+
     this.day = 1;
+
   }
+
+ 
 
   dateToMonth() {
+
     var inputDate = new Date(this.date);
+
     this.month = inputDate.toLocaleDateString('en-us', { month:"short" });
+
   }
+
+ 
 
   dateToDay() {
+
     var inputDate = new Date(this.date);
+
     this.day = inputDate.toLocaleDateString('en-us', { day:"numeric" });
+
   }
 
+ 
+
   render() {
+
+ 
+
     this.dateToMonth();
+
     this.dateToDay();
+
     return html`
+
+ 
+
     <div class="container">
+
       <span class="month">${this.month}</span>
+
       <span class="day">${this.day}</span>
+
     </div>
+
     `;
+
   }
+
 }
+
+ 
 
 customElements.define('date-chip', DateChip);
